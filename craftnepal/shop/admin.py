5b150a04ecbf61f5e592824d0bc405657ff6ab42
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-04-13 11:02:12 +0545

    Customize Django admin panel for product and order management

--- craftnepal/shop/admin.py
+++ craftnepal/shop/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
-from .models import Products,Order
+from .models import Products, Order
 
 # Register your models here.
-admin.site.register(Products)
+admin.site.site_header = "Craft Nepal"
+admin.site.site_title = "Craft Nepal"
+admin.site.index_title = "Manage Craft Nepal"
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category="default")
+
+    change_category_to_default.short_description = "Set category to default"
+    list_display = (
+        "title",
+        "price",
+        "discount_price",
+        "category",
+        "description",
+    )
+    search_fields = (
+        "title",
+        "category",
+    )
+    actions = ("change_category_to_default",)
+    list_editable = ("category","price","discount_price")
+
+
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Order)
--- craftnepal/shop/models.py
+++ craftnepal/shop/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Products(models.Model):
+  
+  def __str__(self):
+    return self.title
+  
   title= models.CharField(max_length=200)
   price=models.FloatField()
   discount_price=models.FloatField()
